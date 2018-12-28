69c0b60b961652ffaabe5b558b6c0c4ffa600044
Ryan Williams <umamimolecule@hotmail.com>
2018-12-29 03:22:37 +0800

    Add left and right navigation.

---
+++ build/precache-manifest.f0ba0c6d7c6b62a18e07271e8cda291e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "8e4251d8d2d43cd6017e",
+    "url": "./static/js/main.8e4251d8.chunk.js"
+  },
+  {
+    "revision": "da6de470eee5e55358fe",
+    "url": "./static/js/1.da6de470.chunk.js"
+  },
+  {
+    "revision": "8e4251d8d2d43cd6017e",
+    "url": "./static/css/main.25a5fcc1.chunk.css"
+  },
+  {
+    "revision": "7104927cb926a01c7a75b05391932129",
+    "url": "./index.html"
+  }
+];
